314275d58a5be501d7a75cc400b3489ea979eb8f
lgornv <igorkaluta1234567@gmail.com>
2025-03-20 12:23:12 +0500

    sdgnkjno

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Main from "./pages/Main";
 import Catalog from "./pages/Catalog";
 import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
